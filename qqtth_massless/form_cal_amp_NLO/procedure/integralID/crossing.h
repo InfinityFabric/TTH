f1942e66fdcdfd8a8a2a0faf5034d8ea4a7a6b98
InfinityFabric <xty2353857486@163.com>
2022-06-01 12:22:02 +0800

    fix prop

--- qqtth_massless/form_cal_amp_NLO/procedure/integralID/crossing.h
+++ qqtth_massless/form_cal_amp_NLO/procedure/integralID/crossing.h
@@ -5,27 +5,27 @@ id q=p1+p2-p3-p4;
 * check the file in reduze/myresults/???.match.info.inc to find crossing rules 
 
 if (match(Sector(pentagon1x12x34,?a)));
-   Multiply replace_(p1,p2,p2,p1,p3,p4,p4,p3);
+*   Multiply replace_(p1,p2,p2,p1,p3,p4,p4,p3);
    id Sector(pentagon1x12x34,?a)=Sector(pentagon1,?a)*cross1x12x34;
 endif;
 
 if (match(Sector(pentagon1x12x345,?a)));
-   Multiply replace_(p2,p1,p4,p2-p4+p1-p3,p1,p2,p3,p4);
+*   Multiply replace_(p2,p1,p4,p2-p4+p1-p3,p1,p2,p3,p4);
    id Sector(pentagon1x12x345,?a)=Sector(pentagon1,?a)*cross1x12x345;
 endif;
 
 if (match(Sector(pentagon1x12x354,?a)));
-   Multiply replace_(p2,p1,p4,p3,p1,p2,p3,p2-p4+p1-p3);
+*   Multiply replace_(p2,p1,p4,p3,p1,p2,p3,p2-p4+p1-p3);
    id Sector(pentagon1x12x354,?a)=Sector(pentagon1,?a)*cross1x12x354;
 endif;
 
 if (match(Sector(pentagon1x12,?a)));
-   Multiply replace_(p1,p2,p2,p1);
+*   Multiply replace_(p1,p2,p2,p1);
    id Sector(pentagon1x12,?a)=Sector(pentagon1,?a)*cross1x12;
 endif;
 
 if (match(Sector(pentagon1x12x45,?a)));
-   Multiply replace_(p2,p1,p4,p2-p4+p1-p3,p1,p2);
+*   Multiply replace_(p2,p1,p4,p2-p4+p1-p3,p1,p2);
    id Sector(pentagon1x12x45,?a)=Sector(pentagon1,?a)*cross1x12x45;
 endif;
 #endprocedure
--- qqtth_massless/form_cal_amp_NLO/procedure/momentumshift/momentumshift_10.h
+++ qqtth_massless/form_cal_amp_NLO/procedure/momentumshift/momentumshift_10.h
@@ -20,6 +20,14 @@
 
 #include ../reduze_NLO/myresults/qqtthoneloop.match.info.inc
 
+id Shift(p1?$i1,p2?$i2,[])=Shift(p1,p2,[]);
+
+id Shift(p1?,p2?,[])=replace_(p1,p2);
+
+Transform prop replace(1,last)=($i1,$i2);
+
+.sort
+
 Splitarg prop;
 id prop(?a,p?kset,?b)=1;
 
@@ -27,6 +35,5 @@ repeat id prop(?a,p1?,p2?,a?)=prop(?a,p1+p2,a);
 id prop(p1?,a?)=prop(p1.p1-a^2);
 
 #include ./procedure/momentumshift/treeproprule.h
-id Shift(p1?,p2?,[])=replace_(p1,p2);
 
 #endprocedure
